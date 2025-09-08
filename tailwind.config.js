93f02358ec0062f1e0ce92d3cf905ae0f3e615b6
Dafrill <talajmagda@gmail.com>
2025-09-08 21:39:00 +0200

    adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Vite potrzebuje ścieżek do plików w projekcie
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
